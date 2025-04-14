eb24ba2d57d740142fef60a82cb2a16dbe11c98f
DenisGoldiner <denys.goldiner@gmail.com>
2025-04-14 19:42:47 +0300

    introduce tracer

--- internal/cmd/term/main.go
+++ internal/cmd/term/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"github.com/DenisGoldiner/webapp/internal/ports/ftp"
+	"github.com/gofiber/fiber/v3"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"golang.org/x/exp/trace"
 	"log"
+	"os"
 
 	"github.com/DenisGoldiner/webapp/internal"
 	"github.com/DenisGoldiner/webapp/internal/adapters/postgres"
@@ -16,6 +19,32 @@ func main() {
 }
 
 func run() {
+
+	fr := trace.NewFlightRecorder()
+	if err := fr.Start(); err != nil {
+		// handle error
+	}
+
+	defer func() {
+		if err := fr.Stop(); err != nil {
+			// handle error
+		}
+	}()
+
+	app := fiber.New()
+
+	app.Get("/trace", func(ctx fiber.Ctx) error {
+		f, err := os.OpenFile("file.out", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
+		if err != nil {
+			// handle error
+		}
+
+		if _, err := fr.WriteTo(f); err != nil {
+			// handle error
+		}
+		return ctx.SendStatus(fiber.StatusOK)
+	})
+
 	dbExec, err := newDB()
 	if err != nil {
 		log.Fatal(err)
@@ -30,6 +59,11 @@ func run() {
 	if err = travellersParser.Run(ctx, "/Users/denys/Go/src/github.com/DenisGoldiner/webapp/internal/integration/data/test_1.csv"); err != nil {
 		log.Printf("error running travellers import: %v", err)
 		return
+	}
+
+	if err := app.Listen(":8080"); err != nil {
+		// handle error
+		app.Shutdown()
 	}
 }
 
